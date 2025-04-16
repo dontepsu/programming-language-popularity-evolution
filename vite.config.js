f596911fbbffcf15d73b8c8e60101eec5ffb1321
Patrik Keinonen <pate@Patriks-MacBook-Pro.local>
2025-04-16 19:39:19 +0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/dontepsu/programming-language-popularity-evolution/',
+  base: '/programming-language-popularity-evolution/',
 });
